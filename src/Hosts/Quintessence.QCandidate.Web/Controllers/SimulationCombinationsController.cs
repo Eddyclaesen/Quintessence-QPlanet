e41bedfaff27f62115d8062a6a2af821f31660c1
Stefan D'hondt <stefan.dhondt@kenze.be>
2020-05-19 11:59:53 +0200

    Added SimulationCombinationsController + setting 'PdfStorageLocation'
    #6875

---
+++ src/Hosts/Quintessence.QCandidate.Web/Controllers/SimulationCombinationsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Quintessence.QCandidate.Controllers
+{
+    [Route("[Controller]")]
+    public class SimulationCombinationsController : Controller
+    {
+        private readonly string _pdfStorageLocation;
+
+        public SimulationCombinationsController(IConfiguration configuration)
+        {
+            _pdfStorageLocation = configuration.GetValue<string>("PdfStorageLocation");
+        }
+
+        [Route("{action}/{simulationCombinationId}/{language}")]
+        public ActionResult GetPdf(Guid simulationCombinationId, string language)
+        {
+            var filename = GetPdfFileLocation(simulationCombinationId, language);
+
+            if(System.IO.File.Exists(filename))
+            {
+                using(var fileStream = new FileStream(filename, FileMode.Open))
+                {
+                    return new FileStreamResult(fileStream, "application/pdf");
+                }
+            }
+
+            return new EmptyResult();
+        }
+
+        private string GetPdfFileLocation(Guid simulationCombinationId, string language)
+        {
+            var filename = $"{simulationCombinationId}.pdf";
+
+            return Path.Combine($"{_pdfStorageLocation}_{language}", filename);
+        }
+    }
+}
