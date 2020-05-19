ea6c345bf61a63f7095cc37887fe2bfb003b4a9b
Stefan D'hondt <stefan.dhondt@kenze.be>
2020-05-19 11:15:53 +0200

    Use hard-coded action parameters for now + actually use mediator.
    #6870

--- src/Hosts/Quintessence.QCandidate.Web/Controllers/AssessmentsController.cs
+++ src/Hosts/Quintessence.QCandidate.Web/Controllers/AssessmentsController.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using MediatR;
+using Quintessence.QCandidate.Core.Queries;
+using System.Threading.Tasks;
 
 namespace Quintessence.QCandidate.Controllers
 {
     [Route("[Controller]")]
     public class AssessmentsController : Controller
     {
-        [Route("/")]
-        [Route("/{candidateId}/{date}")]
-        public IActionResult Get(Guid candidateId, DateTime date)
+        private readonly IMediator _mediator;
+
+        public AssessmentsController(IMediator mediator)
         {
-            return View();
+            _mediator = mediator;
+        }
+
+        [Route("/")]
+        public async Task<IActionResult> Get()
+        {
+            var candidateId = new Guid("CB2ABA03-A397-4DB9-829D-495F26D5E42B");
+            var date = new DateTime(2018, 11, 20);
+            var assessment = await _mediator.Send(new GetAssesmentByCandidateIdAndDateQuery(candidateId, date))
+                .ConfigureAwait(true);
+            return View(assessment);
         }
     }
 }
