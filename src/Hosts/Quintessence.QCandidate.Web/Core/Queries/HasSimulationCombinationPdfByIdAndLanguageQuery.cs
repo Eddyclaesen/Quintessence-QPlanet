db34184be10cd09eb43b34f4c2378df738e41906
Stefan D'hondt <stefan.dhondt@kenze.be>
2020-05-19 14:30:29 +0200

    HasSimulationCombinationPdfByIdAndLanguageQuery(Handler)
    #6875

---
+++ src/Hosts/Quintessence.QCandidate.Web/Core/Queries/HasSimulationCombinationPdfByIdAndLanguageQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Quintessence.QCandidate.Core.Queries
+{
+    public class HasSimulationCombinationPdfByIdAndLanguageQuery : IRequest<bool>
+    {
+        public HasSimulationCombinationPdfByIdAndLanguageQuery(string filename)
+        {
+            Filename = filename;
+        }
+
+        public string Filename { get; }
+    }
+}
---
+++ src/Hosts/Quintessence.QCandidate.Web/Logic/Queries/HasSimulationCombinationPdfByIdAndLanguageQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using MediatR;
+using Quintessence.QCandidate.Core.Queries;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Quintessence.QCandidate.Logic.Queries
+{
+    public class HasSimulationCombinationPdfByIdAndLanguageQueryHandler : IRequestHandler<HasSimulationCombinationPdfByIdAndLanguageQuery, bool>
+    {
+        public Task<bool> Handle(HasSimulationCombinationPdfByIdAndLanguageQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(File.Exists(request.Filename));
+        }
+    }
+}
