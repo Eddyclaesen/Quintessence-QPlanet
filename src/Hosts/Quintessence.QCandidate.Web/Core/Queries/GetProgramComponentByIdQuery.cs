d8fe2d3140a6589c773a93f4bde5c06b85d0fa4c
Stefan D'hondt <stefan.dhondt@kenze.be>
2020-05-25 15:24:13 +0200

    Integrate design
    #6870

---
+++ src/Hosts/Quintessence.QCandidate.Web/Core/Queries/GetProgramComponentByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MediatR;
+using Quintessence.QCandidate.Contracts.Responses;
+
+namespace Quintessence.QCandidate.Core.Queries
+{
+    public class GetProgramComponentByIdQuery : IRequest<ProgramComponentDto>
+    {
+        public GetProgramComponentByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}
