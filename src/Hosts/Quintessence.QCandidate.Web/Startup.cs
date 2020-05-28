c7ee61af4fcfcb5eb8dc5cff921013978dfa2e3e
Stefan D'hondt <stefan.dhondt@kenze.be>
2020-05-28 07:14:30 +0200

    Trial and error
    #6877

--- src/Hosts/Quintessence.QCandidate.Web/Startup.cs
+++ src/Hosts/Quintessence.QCandidate.Web/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Kenze.Infrastructure;
+﻿using System.Globalization;
+using Kenze.Infrastructure;
 using Kenze.Infrastructure.Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Authentication;
@@ -6,11 +7,14 @@ using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Quintessence.QCandidate.Filters.Actions;
 using Quintessence.QCandidate.Logic.Queries;
 using Quintessence.QCandidate.Models;
@@ -37,6 +41,7 @@ namespace Quintessence.QCandidate
                 })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
             services.AddMvcCore(options =>
                 {
                     var policy = new AuthorizationPolicyBuilder()
@@ -44,7 +49,9 @@ namespace Quintessence.QCandidate
                         .Build();
                     options.Filters.Add(new AuthorizeFilter(policy));
                 })
-                .AddRazorViewEngine();
+                .AddRazorViewEngine()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix, opts => { opts.ResourcesPath = "Resources"; });
+
             services.AddMediatR(typeof(GetAssessmentByCandidateIdAndDateQueryHandler).Assembly);
             services.AddScoped<IDbConnectionFactory>(_ =>
                 new SqlDbConnectionFactory(Configuration.GetConnectionString("QPlanet")));
@@ -53,6 +60,21 @@ namespace Quintessence.QCandidate
 
             services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
                 .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("nl"),
+                    new CultureInfo("en"),
+                    new CultureInfo("de"),
+                    new CultureInfo("fr")
+                };
+
+                options.DefaultRequestCulture = new RequestCulture(culture: "nl", uiCulture: "nl");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,6 +89,23 @@ namespace Quintessence.QCandidate
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            //var localizationOption = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+
+            var supportedCultures = new[]
+            {
+                new CultureInfo("nl"),
+                new CultureInfo("en"),
+                new CultureInfo("de"),
+                new CultureInfo("fr")
+            };
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("nl"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
 
             app.UseStaticFiles();
             app.UseHttpsRedirection();
