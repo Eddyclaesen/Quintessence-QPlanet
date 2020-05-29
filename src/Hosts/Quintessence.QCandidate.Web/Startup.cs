4920f23ee697e0bb06019913f37f27b8d9ee23d4
Geert Schreyers <geert.schreyers@kenze.be>
2020-05-29 14:28:16 +0200

    Add multi language support
    #6877

--- src/Hosts/Quintessence.QCandidate.Web/Startup.cs
+++ src/Hosts/Quintessence.QCandidate.Web/Startup.cs
@@ -14,7 +14,6 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using Quintessence.QCandidate.Filters.Actions;
 using Quintessence.QCandidate.Logic.Queries;
 using Quintessence.QCandidate.Models;
@@ -41,7 +40,6 @@ namespace Quintessence.QCandidate
                 })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
-            services.AddLocalization(options => options.ResourcesPath = "Resources");
             services.AddMvcCore(options =>
                 {
                     var policy = new AuthorizationPolicyBuilder()
@@ -50,7 +48,7 @@ namespace Quintessence.QCandidate
                     options.Filters.Add(new AuthorizeFilter(policy));
                 })
                 .AddRazorViewEngine()
-                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix, opts => { opts.ResourcesPath = "Resources"; });
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix/*, opts => { opts.ResourcesPath = "Resources"; }*/);
 
             services.AddMediatR(typeof(GetAssessmentByCandidateIdAndDateQueryHandler).Assembly);
             services.AddScoped<IDbConnectionFactory>(_ =>
@@ -60,21 +58,6 @@ namespace Quintessence.QCandidate
 
             services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
                 .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
-
-            services.Configure<RequestLocalizationOptions>(options =>
-            {
-                var supportedCultures = new[]
-                {
-                    new CultureInfo("nl"),
-                    new CultureInfo("en"),
-                    new CultureInfo("de"),
-                    new CultureInfo("fr")
-                };
-
-                options.DefaultRequestCulture = new RequestCulture(culture: "nl", uiCulture: "nl");
-                options.SupportedCultures = supportedCultures;
-                options.SupportedUICultures = supportedCultures;
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -96,7 +79,6 @@ namespace Quintessence.QCandidate
             {
                 new CultureInfo("nl"),
                 new CultureInfo("en"),
-                new CultureInfo("de"),
                 new CultureInfo("fr")
             };
 
@@ -119,7 +101,6 @@ namespace Quintessence.QCandidate
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Assessments}/{action=Get}");
-                endpoints.MapRazorPages();
             });
 
         }
