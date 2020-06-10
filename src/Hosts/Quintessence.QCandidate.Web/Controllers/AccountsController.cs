661c65920842e19c11108e7c773b8f7a99ad1fad
Stefan D'hondt <stefan.dhondt@kenze.be>
2020-06-10 11:53:33 +0200

    Logout functionality
    #6881

--- src/Hosts/Quintessence.QCandidate.Web/Controllers/AccountsController.cs
+++ src/Hosts/Quintessence.QCandidate.Web/Controllers/AccountsController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Extensions.Options;
+using Quintessence.QCandidate.Models;
 
 namespace Quintessence.QCandidate.Controllers
 {
     public class AccountsController : Controller
     {
+        private readonly Settings _settings;
+
+        public AccountsController(IOptionsMonitor<Settings> settings)
+        {
+            _settings = settings.CurrentValue;
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult SetCulture(string culture, string returnUrl)
@@ -17,6 +30,26 @@ namespace Quintessence.QCandidate.Controllers
                 new CookieOptions { Path = Url.Content("~/") });
 
             return Redirect(returnUrl);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            var scheme = AzureADB2CDefaults.AuthenticationScheme;
+            var authenticated = await HttpContext.AuthenticateAsync(scheme);
+            
+            if(!authenticated.Succeeded)
+            {
+                return Challenge(scheme);
+            }
+
+            await HttpContext.SignOutAsync(AzureADB2CDefaults.AuthenticationScheme);
+
+            //Specify the protocol, so an absolute url is generated, which is needed by azure ad b2c
+            var rootUrl = HttpUtility.UrlEncode(Url.Action("Get", "Assessments", null, Request.Scheme));
+            var redirectUrl = $"{_settings.AzureAdB2C.Tenant}{_settings.AzureAdB2C.Domain}/oauth2/v2.0/logout?p={_settings.AzureAdB2C.SignUpSignInPolicyId}&post_logout_redirect_uri={rootUrl}";
+            
+            return Redirect(redirectUrl);
         }
     }
 }
--- src/Hosts/Quintessence.QCandidate.Web/Models/Settings.cs
+++ src/Hosts/Quintessence.QCandidate.Web/Models/Settings.cs
@@ -3,5 +3,17 @@
     public class Settings
     {
         public string PdfStorageLocation { get; set; }
+        public AzureAdB2C AzureAdB2C { get; set; }
+    }
+
+    public class AzureAdB2C
+    {
+        public string Instance { get; set; }
+        public string Tenant { get; set; }
+        public string ClientId { get; set; }
+        public string Domain { get; set; }
+        public string SignUpSignInPolicyId { get; set; }
+        public string ResetPasswordPolicyId { get; set; }
+        public string EditProfilePolicyId { get; set; }
     }
 }
