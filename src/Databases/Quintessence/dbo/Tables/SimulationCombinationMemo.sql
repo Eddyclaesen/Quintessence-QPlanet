817faa2a5672336f5a7109b1154e81674bafdf93
Geert Schreyers <geert.schreyers@kenze.be>
2020-11-05 07:49:52 +0100

    Fix PR comments.

--- src/Databases/Quintessence/dbo/Tables/SimulationCombinationMemo.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[SimulationCombinationMemo]
-(
-	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
-    [SimulationCombinationId] UNIQUEIDENTIFIER NOT NULL, 
-    [Position] INT NOT NULL,
-    CONSTRAINT [FK_SimulationCombinationMemos_Simulation] FOREIGN KEY ([SimulationCombinationId]) REFERENCES [dbo].[SimulationCombination] ([Id]),
-    INDEX [IX_SimulationCombination_Position] CLUSTERED ([SimulationCombinationId],[Position])
-)
