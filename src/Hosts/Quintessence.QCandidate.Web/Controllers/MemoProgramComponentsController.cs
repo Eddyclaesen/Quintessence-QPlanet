dd91a55541a94668e0d7cace6840a44a07b0a8a7
Geert Schreyers <geert.schreyers@kenze.be>
2020-11-10 13:21:13 +0100

    Add a basic model to the view and retrieve the FunctionsDescription from disk

--- src/Hosts/Quintessence.QCandidate.Web/Controllers/MemoProgramComponentsController.cs
+++ src/Hosts/Quintessence.QCandidate.Web/Controllers/MemoProgramComponentsController.cs
@@ -1,6 +1,11 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Quintessence.QCandidate.Configuration;
+using Quintessence.QCandidate.Models.MemoProgramComponents;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 
@@ -9,17 +14,29 @@ namespace Quintessence.QCandidate.Controllers
     [Route("[Controller]")]
     public class MemoProgramComponentsController : Controller
     {
+        private const string FunctionDescriptionsFolder = "FunctionDescriptions";
+
         private readonly IMediator _mediator;
-        public MemoProgramComponentsController(IMediator mediator)
+        private readonly string _htmlStorageLocation;
+
+        public MemoProgramComponentsController(IMediator mediator, IOptionsMonitor<Settings> optionsMonitor)
         {
             _mediator = mediator;
+            _htmlStorageLocation = optionsMonitor.CurrentValue.HtmlStorageLocation;
         }
 
         [Route("{action}/{id}")]
         public async Task<IActionResult> Details(Guid id)
         {
+            id = Guid.Parse("01FBB298-AE9A-4BFF-BD9A-A2750FF5A0B5");
 
-            return View();
+            var basePath = Path.Combine(_htmlStorageLocation, id.ToString());
+            var language = HttpContext.Features.Get<IRequestCultureFeature>().RequestCulture.UICulture.Name;
+            var functionDescription = System.IO.File.ReadAllText(Path.Combine(basePath, FunctionDescriptionsFolder, $"{language.ToUpperInvariant()}.html"));
+
+            var model = new MemoProgramComponent(id, functionDescription);
+
+            return View(model);
         }
     }
 }
---
+++ src/Hosts/Quintessence.QCandidate.Web/Models/MemoProgramComponents/MemoProgramComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Quintessence.QCandidate.Models.MemoProgramComponents
+{
+    public class MemoProgramComponent
+    {
+        public MemoProgramComponent(Guid id, string functionDescription)
+        {
+            Id = id;
+            FunctionDescription = functionDescription;
+        }
+
+        public Guid Id { get; }
+
+        public string FunctionDescription { get; }
+    }
+}
