553d101b3cbfe144cec800501d020aef00958465
Guy Heylens <guy@mosp.be>
2020-11-13 14:24:06 +0100

    Merge issue
    Related Work Items: #7791

--- src/Hosts/Quintessence.QCandidate.Web/Controllers/MemoProgramComponentsController.cs
+++ src/Hosts/Quintessence.QCandidate.Web/Controllers/MemoProgramComponentsController.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using Quintessence.QCandidate.Contracts.Responses;
+using Quintessence.QCandidate.Core.Queries;
 using MemoProgramComponent = Quintessence.QCandidate.Models.MemoProgramComponents.MemoProgramComponent;
 
 
