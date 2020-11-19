44bb86fdf933ade8677fc69c364d7868a8b57745
Geert Schreyers <geert.schreyers@kenze.be>
2020-11-19 20:03:10 +0100

    Show the intro with a gray background when there is a predecessor. Show new memos with a gray background when there is a predecessor.

--- src/Hosts/Quintessence.QCandidate.Web/Models/MemoProgramComponents/Memo.cs
+++ src/Hosts/Quintessence.QCandidate.Web/Models/MemoProgramComponents/Memo.cs
@@ -4,17 +4,19 @@ namespace Quintessence.QCandidate.Models.MemoProgramComponents
 {
     public class Memo
     {
-        public Memo(Guid id, int position, string title, string content)
+        public Memo(Guid id, int position, string title, string content, bool hasPredecessorOrigin = false)
         {
             Id = id;
             Position = position;
             Title = title;
             Content = content;
+            HasPredecessorOrigin = hasPredecessorOrigin;
         }
 
         public Guid Id { get; }
         public int Position { get; }
         public string Title { get; }
         public string Content { get; }
+        public bool HasPredecessorOrigin { get; }
     }
 }
